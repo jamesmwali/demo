1c9e557f1d491ed90a9e2f8907cfe048be174c74
James Mwali <jamessmwali@gmail.com>
2016-09-14 20:00:07 +0200

    login service script for getting auth token

---
+++ app/scripts/services/login.service.js
@@ -0,0 +1,85 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.login')
+        .factory('AuthService', AuthService);
+
+
+        function AuthService($log, $rootScope, $location, $http, $q, $cookieStore, $window){
+          var service;
+          var user;
+
+          if(this.currentUser === undefined)
+          service.currentUser = {};
+
+          service.isUserDefined = function(){
+            user = JSON.parse($window.localStorage.currentUser || null);
+            $log.log("User", isUserDefined);
+            if(!user) {
+
+              $location.path("/");
+            } else{
+              $rootScope.currentUser = user;
+              $rootScope.loginClass = "";
+            }
+          } ;
+
+          service.setUser = function(user, $q){
+
+            $log.log("Setting new user");
+
+            // persistant
+            $window.localStorage.currentUser = JSON.stringify(user);
+            $cookieStore.put('globals', user);
+
+            //won't be retained after refresh
+            service.currentUser = user;
+            $rootScope.currentUser = user;
+
+
+          };
+
+          service.login = function(username, password){
+
+              //Request payload
+
+              var data = {};
+
+              data = {
+
+                'username': username,
+                'password': password,
+              };
+
+              $log.log($http.headers);
+
+              return $http({
+
+                method: 'post',
+                url: '//userservice.staging.tangentmicroservices.com:80/api­token­auth/',
+                data: data,
+
+              }).then(function(response) {
+
+                // Setting the token to be used in the request
+                $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.access_token;
+                $log.log("Testing token:", response.data);
+                $log.log("Testing token 2:", response.access_token);
+
+                user = response.data;
+                return response;
+
+              }, function(response){
+                $log.log("response", response);
+              }) // login
+
+          }
+
+
+        }
+
+
+
+
+});

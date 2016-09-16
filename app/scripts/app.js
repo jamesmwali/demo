12f15fec1b701db25bddcfcd8cd5e08e4b76636c
James Mwali <jamessmwali@gmail.com>
2016-09-16 18:32:59 +0200

    attempt on redirected unauth users to login page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,10 +34,26 @@ angular
       })
       .when('/view', {
         templateUrl: 'views/view.html',
-        controller: 'ViewCtrl',
-        controllerAs: 'view'
+        controller: 'AddUserCtrl',
+        controllerAs: 'add'
       })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+
+  .run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+        // $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        //     // redirect to login page if not logged in
+        //     if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+        //         $location.path('/login');
+        //     }
+        // });
+    }]);

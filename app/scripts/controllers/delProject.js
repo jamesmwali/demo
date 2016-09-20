9a6d935a251aaa6c7e1768c5da0e27b276240b4c
James Mwali <jamessmwali@gmail.com>
2016-09-20 20:42:59 +0200

    controller for editing and deleting a project

---
+++ app/scripts/controllers/delProject.js
@@ -0,0 +1,15 @@
+(function(){
+
+  'use strict';
+  angular.module('app.login')
+  .controller('ProjectDelCtrl', ProjectDelCtrl);
+
+  function ProjectDelCtrl($log, $scope, $rootScope, $cookieStore, AuthService, $http, ProjectService){
+
+    
+
+
+
+  }
+
+})();
---
+++ app/scripts/controllers/editProject.js
@@ -0,0 +1,15 @@
+(function(){
+
+  'use strict';
+  angular.module('app.login')
+  .controller('ProjectEditCtrl', ProjectEditCtrl);
+
+  function ProjectEditCtrl($log, $scope, $rootScope, $cookieStore, AuthService, $http, ProjectService){
+    
+
+
+
+
+  }
+
+})();

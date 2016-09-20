eaa98aa3db6b9615c1c21c793f84e2899f13e275
James Mwali <jamessmwali@gmail.com>
2016-09-20 23:29:03 +0200

    minor hack in authenticting users

--- app/scripts/services/project.services.js
+++ app/scripts/services/project.services.js
@@ -10,93 +10,135 @@ angular.module('app.login')
       var service = {};
       var data;
 
-      service.editProject = function(pk){
+      service.editProject = function(pk, data){
 
-        var defer = $q.defer();
-        var url = '//projectservice.staging.tangentmicroservices.com/api/v1/projects/'+pk+'/';
+        var url = '//projectservice.staging.tangentmicroservices.com:80/api/v1/projects/'+ pk +'/';
 
-        $http({
+        return $http({
                 method: 'put',
-                url: url
-             }).success(function(response){
-                console.log("Fetching project Info ");
-                  defer.resolve(response);
-             }).error(function(err, status){
-                console.log("Oops something went wrong");
-                  defer.reject(err);
+                url: url,
+                data: data
+             }).then(function(response){
+                console.log("Edited project Successfully ", response);
+                  // defer.resolve(response);
+                  return response
+             },function(err){
+                console.log("Oops something went wrong", err);
+                  // defer.reject(err);
+                  if(err.data.detail === "No such user"){
+
+                    $location.path("/login");
+                  }
 
              });
-
-
-        return defer.promise;
 
       }
 
       service.getProject = function(pk){
-
-        var defer = $q.defer();
-        var url = '//projectservice.staging.tangentmicroservices.com/api/v1/projects/'+pk+'/';
-
-        $http({
-                method: 'get',
-                url: url
-             }).success(function(response){
-                console.log("Fetching project ");
-                  defer.resolve(response);
-             }).error(function(err, status){
-                console.log("Oops something went wrong");
-                  defer.reject(err);
-
-             });
+        var url = '//projectservice.staging.tangentmicroservices.com:80/api/v1/projects/'+pk+'/';
 
 
-        return defer.promise;
+          return $http({
+                  method: 'get',
+                  url: url,
+                  data: data
+               }).then(function(response){
+                  console.log("Fetched project Successfully ", response);
+                    // defer.resolve(response);
+                    return response
+               },function(err){
+                  console.log("Oops something went wrong", err);
+                    // defer.reject(err);
+                    if(err.data.detail === "No such user"){
+
+                      $location.path("/login");
+                    }
+               });
 
         }
 
 
-        service.editProjectV1 = function(pk){
+        service.editProjectV1 = function(pk, data){
 
               var defer = $q.defer();
-              var url = '//projectservice.staging.tangentmicroservices.com/api/v1/projects/'+pk+'/';
+              var url = '//projectservice.staging.tangentmicroservices.com:80/api/v1/projects/'+pk+'/';
 
-              $http({
+
+              return $http({
                       method: 'patch',
-                      url: url
-                   }).success(function(response){
-                      console.log("edited project ");
-                        defer.resolve(response);
-                   }).error(function(err, status){
-                      console.log("Oops something went wrong");
-                        defer.reject(err);
+                      url: url,
+                      data: data
+                   }).then(function(response){
+                      console.log("Edited project Successfully ", response);
+                        // defer.resolve(response);
+                        return response
+                   },function(err){
+                      console.log("Oops something went wrong", err);
+                      if(err.data.detail === "No such user"){
+
+                        $location.path("/login");
+                      }
+                        // defer.reject(err);
 
                    });
-
-
-              return defer.promise;
 
         }
 
 
         service.deleteProject = function(pk){
 
-                var defer = $q.defer();
-                var url = '//projectservice.staging.tangentmicroservices.com/api/v1/projects/'+pk+'/';
+                var url = '//projectservice.staging.tangentmicroservices.com:80/api/v1/projects/'+pk+'/';
 
-                $http({
+                return $http({
                         method: 'delete',
-                        url: url
-                     }).success(function(response){
-                        console.log("Deleted project ");
-                          defer.resolve(response);
-                     }).error(function(err, status){
-                        console.log("Oops something went wrong");
-                          defer.reject(err);
+                        url: url,
+                        data: data
+                     }).then(function(response){
+                        console.log("Deleted project Successfully ", response);
+                          // defer.resolve(response);
+                          return response
+                     },function(err){
+                        console.log("Oops something went wrong", err);
+                        if(err.data.detail === "No such user"){
+
+                          $location.path("/login");
+                        }
+                          // defer.reject(err);
 
                      });
-
-                return defer.promise;
               }
+
+
+              service.getNewProject = function(){
+                return service.new_project || null;
+              };
+              service.setNewProject = function(obj){
+                service.new_project = obj;
+              };
+
+              service.getNewPk = function(){
+                return service.new_pk || null;
+              };
+
+              service.setNewPk = function(obj){
+                service.new_pk = obj;
+              };
+
+              service.getProjName = function(){
+                return service.new_name || null;
+              };
+
+              service.setProjName = function(obj){
+                service.new_name = obj;
+              };
+
+              service.getProjDesc = function(){
+                return service.new_desc || null;
+              };
+
+              service.setProjDesc = function(obj){
+                service.new_desc = obj;
+              };
 
       return service;
 

c6d8ad2dff4688b9731c6848bff12415a9b962e3
James Mwali <jamessmwali@gmail.com>
2016-09-20 23:14:06 +0200

    added controller for deleting a project

--- app/scripts/controllers/delProject.js
+++ app/scripts/controllers/delProject.js
@@ -6,7 +6,38 @@
 
   function ProjectDelCtrl($log, $scope, $rootScope, $cookieStore, AuthService, $http, ProjectService){
 
-    
+
+        $scope.deleteProject = function(){
+
+
+          if($scope.pk!=="" || $scope.pk!== "undefined"){
+
+            var pk = $scope.pk;
+            var data = {
+              "pk": $scope.pk,
+            };
+
+            ProjectService.deleteProject($scope.pk, data).
+            then(function(response){
+              if(response){
+                $log.log("Project Deleted Successfully");
+                $log.log(response);
+                $scope.SuccessMessage = "Project Deleted Successfully";
+              }else{
+                $scope.Errormsg = "Primary Key is Invalid/ Does not exist, Try Again";
+              }
+            }, function(response){
+                $log.log("failed", response);
+                $scope.Errormsg = "Primary Key is Invalid/ Does not exist, Try Again";
+
+            });
+
+          }else {
+
+              $scope.Required = "Project Primary Key (PK) is Required";
+          }
+
+        };
 
 
 
